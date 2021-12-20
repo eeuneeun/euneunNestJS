2dc219b45abe93d067429626eed87d6d621f6511
eeuneeun <sacroo89@gmail.com>
2021-12-20 18:00:53 +0900

    마지막 커밋

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app/app.controller';
+import { UserController } from './user/user.controller';
 
 // * module
 // 서비스를 만드는 데에 필요한 모든 것들이 들어있음!
 
 @Module({
   imports: [MoviesModule],
-  controllers: [AppController],
+  controllers: [AppController, UserController],
   providers: [],
 })
 export class AppModule {}
